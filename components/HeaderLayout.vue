3987ece25f5678dc1bc500a061d2de9fb3ffea3d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 16:18:05 +0100

    dev: added connect button with heroicon

--- components/HeaderLayout.vue
+++ components/HeaderLayout.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import AuthConnection from "./authentication/AuthConnection.vue";
+</script>
+
 <template>
-  <header class="flex h-12 items-center bg-gray-100">
+  <header class="flex h-12 items-center justify-between bg-gray-100">
     <NuxtLink
       to="/"
       class="flex items-center gap-2 rounded-lg px-2 hover:bg-gray-200"
@@ -7,5 +11,6 @@
       <img src="/logo.svg" alt="Logo" class="h-10" />
       <span class="text-xl font-bold">Cartes</span>
     </NuxtLink>
+    <AuthConnection />
   </header>
 </template>
---
+++ components/authentication/AuthConnection.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {
+  BeakerIcon,
+  ArrowRightEndOnRectangleIcon,
+} from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <NuxtLink to="/login" class="btn btn-neutral btn-sm mx-2 rounded-full">
+    <ArrowRightEndOnRectangleIcon class="size-6" />
+    <span>Connexion</span>
+  </NuxtLink>
+</template>
