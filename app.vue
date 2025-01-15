06599ed161f93a0290e1fedc8114fee4cfdf7664
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 11:38:00 +0100

    dev: using the headerlayout
    create the components directory
    move the component header inside
    
    the component can be used globally.

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="bg-gray-100">
-    <h1>Welcome to the homepage</h1>
-  </div>
+  <HeaderLayout />
 </template>
---
+++ components/HeaderLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="bg-gray-100">
+    <a href="#">
+      <img src="/logo.svg" alt="Logo" class="h-10" />
+      <span>Cartes</span>
+    </a>
+  </header>
+</template>
