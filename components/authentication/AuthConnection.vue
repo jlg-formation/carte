c9ef9737adf33d593154c0daa115010eea6d815b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 17:36:15 +0100

    dev: use computed and get route name

--- components/authentication/AuthConnection.vue
+++ components/authentication/AuthConnection.vue
@@ -1,13 +1,17 @@
 <script setup>
-import {
-  BeakerIcon,
-  ArrowRightEndOnRectangleIcon,
-} from "@heroicons/vue/24/solid";
+import { ArrowRightEndOnRectangleIcon } from "@heroicons/vue/24/solid";
+
+const route = useRoute();
+console.log("route: ", route);
+
+const isLoginPage = computed(() => route.name === "login");
 </script>
 
 <template>
-  <NuxtLink to="/login" class="btn btn-neutral btn-sm mx-2 rounded-full">
-    <ArrowRightEndOnRectangleIcon class="size-6" />
-    <span>Connexion</span>
-  </NuxtLink>
+  <template v-if="!isLoginPage">
+    <NuxtLink to="/login" class="btn btn-neutral btn-sm mx-2 rounded-full">
+      <ArrowRightEndOnRectangleIcon class="size-6" />
+      <span>Connexion</span>
+    </NuxtLink>
+  </template>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { EnvelopeIcon } from "@heroicons/vue/24/outline";
+import { KeyIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <main class="page page-center">
+    <h1 class="page-title">Se connecter</h1>
+    <form class="flex w-full max-w-xs flex-col gap-2">
+      <label class="input input-bordered flex items-center gap-2">
+        <EnvelopeIcon class="size-6 text-gray-500" />
+        <input type="email" placeholder="E-mail" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2">
+        <KeyIcon class="size-6 text-gray-500" />
+        <input type="email" placeholder="Password" />
+      </label>
+      <button class="btn btn-primary" disabled>Se connecter</button>
+    </form>
+    <NuxtLink to="/new-account" class="btn btn-link font-normal text-black"
+      >Pas encore de compte ?</NuxtLink
+    >
+  </main>
+</template>
