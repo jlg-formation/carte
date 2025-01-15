9befb6a51897e6cdebb57e5b5dfb7e00b6ecc201
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 13:43:17 +0100

    dev: ez

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="page page-center">
     <h1 class="mb-8 text-center text-4xl font-bold">
-      Indiquer vos lieus favoris sur des cartes
+      Indiquez vos lieus favoris sur des cartes.
     </h1>
     <NuxtLink to="/carte" class="btn btn-primary">Voir les lieux</NuxtLink>
   </main>
