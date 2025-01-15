ae81e87f05294379824bf498440ba5457949fb80
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 11:04:35 +0100

    dev: using NuxtLink to navigate

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>Welcome !</h1>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/legal">Mentions Légales</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
 </template>
