23a8653583eec6f07074b28723e751199b69e2df
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-15 12:09:19 +0100

    dev: added daisy UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
     <h1>Indiquer vos lieus favoris sur des cartes</h1>
-    <NuxtLink to="/carte">Voir les lieux</NuxtLink>
+    <NuxtLink to="/carte" class="btn">Voir les lieux</NuxtLink>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,5 +13,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
