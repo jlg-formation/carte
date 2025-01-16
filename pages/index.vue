6fc248f43a19a2d30ce1e55eef2cec043be3f773
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-16 12:16:33 +0100

    dev: hero daisyUI

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,20 @@ import { MapIcon } from "@heroicons/vue/24/solid";
 
 <template>
   <main class="page page-center">
-    <h1 class="mb-8 text-center text-4xl font-bold">
-      Indiquez vos lieus favoris sur des cartes.
-    </h1>
-    <NuxtLink to="/carte" class="btn btn-primary">
-      <MapIcon class="size-6" />
-      <span>Voir les lieux</span>
-    </NuxtLink>
+    <div class="hero">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Vos endroits sur des cartes.</h1>
+          <p class="py-6">
+            Produisez des cartes avec des lieux intéressant et partagez les !
+          </p>
+
+          <NuxtLink to="/carte" class="btn btn-primary">
+            <MapIcon class="size-6" />
+            <span>Voir les lieux</span>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
