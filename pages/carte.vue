95f2a983da9d577bbf7a3aab9575b925264234cb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-16 10:27:37 +0100

    dev: daisyui drawer operationnal

--- pages/carte.vue
+++ pages/carte.vue
@@ -1,8 +1,41 @@
+<script setup lang="ts">
+import { ChevronRightIcon, XMarkIcon } from "@heroicons/vue/24/solid";
+</script>
+
 <template>
-  <main class="page">
-    <div class="h-full w-full">
-      <MapManager />
-      <MapDisplay />
+  <main class="flex-grow p-2">
+    <div class="drawer">
+      <input id="daisyui-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <label
+          for="daisyui-drawer"
+          class="btn btn-circle drawer-button absolute bg-white shadow-md"
+        >
+          <ChevronRightIcon class="size-6" />
+        </label>
+        <MapDisplay />
+      </div>
+      <div class="drawer-side">
+        <label
+          for="daisyui-drawer"
+          aria-label="close sidebar"
+          class="drawer-overlay"
+        ></label>
+        <div class="flex h-full w-72 flex-col">
+          <label for="daisyui-drawer" class="drawer-button flex h-12"> </label>
+          <div class="flex flex-grow flex-col bg-white p-2">
+            <label
+              for="daisyui-drawer"
+              class="btn btn-circle drawer-button bg-white shadow-md"
+            >
+              <XMarkIcon class="size-6" />
+            </label>
+            <div class="flex-grow">
+              <MapManager />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
