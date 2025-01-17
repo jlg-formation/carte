61bc920c8b23dcabea572965e9a400e3c665ecc6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-17 16:57:54 +0100

    dev: added buttons import export

--- components/map/MapManager.vue
+++ components/map/MapManager.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/vue/24/solid";
+
 const { places } = storeToRefs(usePlaceStore());
 </script>
 
 <template>
   <div class="flex flex-col gap-2 pt-4">
+    <nav class="flex gap-2">
+      <button class="btn">
+        <ArrowDownTrayIcon class="size-6" />
+        <span>Import</span>
+      </button>
+      <button class="btn">
+        <ArrowUpTrayIcon class="size-6" />
+        <span>Export</span>
+      </button>
+    </nav>
     <div v-for="place in places" :key="place.id" class="rounded-lg border p-2">
       <div class="font-bold">{{ place.name }}</div>
       <div>
