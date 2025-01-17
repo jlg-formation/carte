a1526b478f210ca522b8b66f0049711421d07ab7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-17 16:39:56 +0100

    dev: list places

--- components/map/MapManager.vue
+++ components/map/MapManager.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const { places } = storeToRefs(usePlaceStore());
+</script>
+
 <template>
-  <h1>Map Manager</h1>
+  <div class="flex flex-col gap-2 pt-4">
+    <div v-for="place in places" :key="place.id" class="rounded-lg border p-2">
+      <div class="font-bold">{{ place.name }}</div>
+      <div>
+        {{
+          `${place.gps.latitude.toFixed(5)}, ${place.gps.longitude.toFixed(5)}`
+        }}
+      </div>
+    </div>
+  </div>
 </template>
