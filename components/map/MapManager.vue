fe822620ec0d10e38bd8b4dd44cd01cf428befe3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-20 18:19:16 +0100

    dev: added MapBlocButtons

--- components/map/MapManager.vue
+++ components/map/MapManager.vue
@@ -1,31 +1,11 @@
 <script setup lang="ts">
-import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/vue/24/solid";
-
 const placeStore = usePlaceStore();
 const { places } = storeToRefs(placeStore);
-
-const handleImport = async () => {
-  console.log("handleImport");
-  await placeStore.importJson();
-};
-const handleExport = async () => {
-  console.log("handleExport");
-  await placeStore.exportJson();
-};
 </script>
 
 <template>
   <div class="flex flex-col gap-2 pt-4">
-    <nav class="flex gap-2">
-      <button class="btn" @click="handleImport">
-        <ArrowDownTrayIcon class="size-6" />
-        <span>Import</span>
-      </button>
-      <button class="btn" @click="handleExport">
-        <ArrowUpTrayIcon class="size-6" />
-        <span>Export</span>
-      </button>
-    </nav>
+    <MapBlocButtons />
     <div v-for="place in places" :key="place.id" class="rounded-lg border p-2">
       <div class="font-bold">{{ place.name }}</div>
       <div>
---
+++ components/map/bloc/MapBlocButtons.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/vue/24/solid";
+
+const placeStore = usePlaceStore();
+
+const handleImport = async () => {
+  console.log("handleImport");
+  await placeStore.importJson();
+};
+const handleExport = async () => {
+  console.log("handleExport");
+  await placeStore.exportJson();
+};
+</script>
+
+<template>
+  <nav class="flex gap-2">
+    <button class="btn" @click="handleImport">
+      <ArrowDownTrayIcon class="size-6" />
+      <span>Import</span>
+    </button>
+    <button class="btn" @click="handleExport">
+      <ArrowUpTrayIcon class="size-6" />
+      <span>Export</span>
+    </button>
+  </nav>
+</template>
