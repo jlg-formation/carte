6315bb1d6f945f61f9a0ae272b046ebcec6a410d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-20 18:37:15 +0100

    dev: composable

--- components/map/bloc/MapBlocButtons.vue
+++ components/map/bloc/MapBlocButtons.vue
@@ -2,24 +2,15 @@
 import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/vue/24/solid";
 
 const placeStore = usePlaceStore();
-
-const handleImport = async () => {
-  console.log("handleImport");
-  await placeStore.importJson();
-};
-const handleExport = async () => {
-  console.log("handleExport");
-  await placeStore.exportJson();
-};
 </script>
 
 <template>
   <nav class="flex gap-2">
-    <button class="btn" @click="handleImport">
+    <button class="btn" @click="placeStore.importJson()">
       <ArrowDownTrayIcon class="size-6" />
       <span>Import</span>
     </button>
-    <button class="btn" @click="handleExport">
+    <button class="btn" @click="placeStore.exportJson()">
       <ArrowUpTrayIcon class="size-6" />
       <span>Export</span>
     </button>
