275c3d5c38f3570dd7d1188e7a6b69224d5d8ab3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-20 18:22:29 +0100

    dev: added mapblocplacelist
    delegation

--- components/map/MapManager.vue
+++ components/map/MapManager.vue
@@ -1,18 +1,6 @@
-<script setup lang="ts">
-const placeStore = usePlaceStore();
-const { places } = storeToRefs(placeStore);
-</script>
-
 <template>
   <div class="flex flex-col gap-2 pt-4">
     <MapBlocButtons />
-    <div v-for="place in places" :key="place.id" class="rounded-lg border p-2">
-      <div class="font-bold">{{ place.name }}</div>
-      <div>
-        {{
-          `${place.gps.latitude.toFixed(5)}, ${place.gps.longitude.toFixed(5)}`
-        }}
-      </div>
-    </div>
+    <MapBlocPlaceList />
   </div>
 </template>
---
+++ components/map/bloc/MapBlocPlaceList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const placeStore = usePlaceStore();
+const { places } = storeToRefs(placeStore);
+</script>
+
+<template>
+  <div v-for="place in places" :key="place.id" class="rounded-lg border p-2">
+    <div class="font-bold">{{ place.name }}</div>
+    <div>
+      {{
+        `${place.gps.latitude.toFixed(5)}, ${place.gps.longitude.toFixed(5)}`
+      }}
+    </div>
+  </div>
+</template>
