91eb92591b3b79f3a3e52428baf4e64d02a3cc23
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-21 20:03:57 +0100

    dev: delegation mapblocmenunormal

--- components/map/bloc/MapBlocMenu.vue
+++ components/map/bloc/MapBlocMenu.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import { MapPinIcon, PlusIcon } from "@heroicons/vue/24/solid";
-
-const { menuStyle, selectedPlace, isAdding, name, gpsCoord } =
-  useMapDisplayState();
-const { copyGpsCoord, addPlace, addPlace2 } = useMapDisplayHandlers();
+const { menuStyle } = useMapDisplayState();
 </script>
 
 <template>
@@ -12,29 +8,6 @@ const { copyGpsCoord, addPlace, addPlace2 } = useMapDisplayHandlers();
     :style="menuStyle"
   >
     <MapBlocMenuSelected />
-    <template v-if="!selectedPlace">
-      <li>
-        <a @click="copyGpsCoord">
-          <MapPinIcon class="size-6 text-neutral-500" />
-          <span>{{ gpsCoord }}</span>
-        </a>
-      </li>
-      <li>
-        <a v-if="!isAdding" @click="addPlace">
-          <PlusIcon class="size-6 text-neutral-500" />
-          <span>Ajouter un lieu</span>
-        </a>
-        <form v-else @submit.prevent="addPlace2">
-          <input
-            v-model="name"
-            v-focus
-            placeholder="Nom"
-            type="text"
-            class="input input-sm input-bordered"
-            required
-          />
-        </form>
-      </li>
-    </template>
+    <MapBlocMenuNormal />
   </ul>
 </template>
---
+++ components/map/bloc/MapBlocMenuNormal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { MapPinIcon, PlusIcon } from "@heroicons/vue/24/solid";
+
+const { selectedPlace, isAdding, name, gpsCoord } = useMapDisplayState();
+const { copyGpsCoord, addPlace, addPlace2 } = useMapDisplayHandlers();
+</script>
+
+<template>
+  <template v-if="!selectedPlace">
+    <li>
+      <a @click="copyGpsCoord">
+        <MapPinIcon class="size-6 text-neutral-500" />
+        <span>{{ gpsCoord }}</span>
+      </a>
+    </li>
+    <li>
+      <a v-if="!isAdding" @click="addPlace">
+        <PlusIcon class="size-6 text-neutral-500" />
+        <span>Ajouter un lieu</span>
+      </a>
+      <form v-else @submit.prevent="addPlace2">
+        <input
+          v-model="name"
+          v-focus
+          placeholder="Nom"
+          type="text"
+          class="input input-sm input-bordered"
+          required
+        />
+      </form>
+    </li>
+  </template>
+</template>
