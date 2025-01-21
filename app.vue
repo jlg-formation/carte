e57e44a20d049735bd1ae71c95e96d84fbe5c379
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-21 17:42:38 +0100

    dev: default vue should have one root element (eslint)

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="relative z-0 flex min-h-screen flex-col justify-between">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <HeaderLayout class="relative z-0" />
-  <slot />
-  <FooterLayout class="relative z-0" />
+  <div class="relative z-0 flex min-h-screen flex-col justify-between">
+    <HeaderLayout class="relative z-0" />
+    <slot />
+    <FooterLayout class="relative z-0" />
+  </div>
 </template>
