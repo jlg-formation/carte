f76f10366c013aebd2958b041fb79ce2e0f3c05b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-27 12:25:43 +0100

    dev: input password with eye

--- components/input/InputPassword.vue
+++ components/input/InputPassword.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import { KeyIcon } from "@heroicons/vue/24/solid";
+import { EyeIcon, EyeSlashIcon, KeyIcon } from "@heroicons/vue/24/solid";
 
 const password = defineModel<string>();
+
+const isVisible = ref(false);
 </script>
 
 <template>
-  <label class="input input-bordered flex items-center gap-2">
+  <label class="input input-bordered flex items-center justify-between gap-2">
     <KeyIcon class="size-6 text-gray-500" />
-    <input v-model="password" type="password" placeholder="Password" />
+    <input
+      v-model="password"
+      :type="isVisible ? 'text' : 'password'"
+      placeholder="Password"
+      class="flex-grow"
+    />
+    <span @click="isVisible = !isVisible">
+      <EyeIcon v-if="isVisible" class="size-6 text-gray-500" />
+      <EyeSlashIcon v-else class="size-6 text-gray-500" />
+    </span>
   </label>
 </template>
