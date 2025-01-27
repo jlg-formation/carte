14460ba289ae1e4e36da00fdbf2daaf92faae3a9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-27 12:41:33 +0100

    dev: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,6 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default withNuxt(eslintConfigPrettier, {
   rules: {
     "max-lines": ["error", { max: 65 }],
+    "vue/attribute-hyphenation": "off",
   },
 });
