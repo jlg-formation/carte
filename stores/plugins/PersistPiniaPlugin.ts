fbab50d367a9effe2fd029ab7c2a94f8ddad84c4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-28 10:17:11 +0100

    dev: added pinia plugin

---
+++ stores/plugins/PersistPiniaPlugin.ts
@@ -0,0 +1,28 @@
+import { clone } from "~/utils/clone";
+import localforage from "localforage";
+import type { PiniaPlugin } from "pinia";
+
+const sendEvent = (storeId: string) => {
+  document.body.dispatchEvent(new Event(storeId + "_ready"));
+};
+
+export const persistPiniaPlugin: PiniaPlugin = ({ store }) => {
+  (async () => {
+    const key = `piniaState.${store.$id}`;
+
+    store.$subscribe((mutation, state) => {
+      (async () => {
+        await localforage.setItem(key, clone(state));
+      })();
+    });
+
+    const obj: object | null = await localforage.getItem(key);
+    if (obj === null) {
+      localforage.setItem(key, clone(store.$state));
+      sendEvent(store.$id);
+      return;
+    }
+    store.$patch(obj);
+    sendEvent(store.$id);
+  })();
+};
