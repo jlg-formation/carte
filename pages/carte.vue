916c27a1947d847ca103e8d9e2599c206fd9fdf3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-28 15:02:43 +0100

    dev: replace .client.vue by ClientOnly

---
+++ pages/carte.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ChevronRightIcon, XMarkIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <ClientOnly>
+    <main class="relative -z-10 flex flex-grow">
+      <div class="drawer">
+        <input id="daisyui-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+          <label
+            for="daisyui-drawer"
+            class="btn btn-circle drawer-button absolute z-[9999] m-2 bg-base-100 shadow-md"
+          >
+            <ChevronRightIcon class="size-6" />
+          </label>
+          <MapDisplay />
+        </div>
+        <div class="drawer-side z-[9999]">
+          <label
+            for="daisyui-drawer"
+            aria-label="close sidebar"
+            class="drawer-overlay"
+          ></label>
+          <div class="flex h-full w-72 flex-col">
+            <label for="daisyui-drawer" class="drawer-button flex h-12">
+            </label>
+            <div class="flex flex-grow flex-col bg-base-100 p-2">
+              <label
+                for="daisyui-drawer"
+                class="btn btn-circle drawer-button bg-base-100 shadow-md"
+              >
+                <XMarkIcon class="size-6" />
+              </label>
+              <div class="flex-grow">
+                <MapManager />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </ClientOnly>
+</template>
