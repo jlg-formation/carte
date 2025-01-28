50407a33966ded4bce3d763add97cde99494da3a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-01-28 19:27:41 +0100

    dev: promises

--- examples/03-async/06-promises-ootb.cjs
+++ examples/03-async/06-promises-ootb.cjs
@@ -1,18 +1,5 @@
 const fs = require("fs");
-
-const promisify =
-  (fn) =>
-  (...args) => {
-    return new Promise((resolve, reject) => {
-      fn(...args, (err, result) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-      });
-    });
-  };
+const { promisify } = require("util");
 
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
