850286afc867429357022fca8a1919cc7e0e4168
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-02-03 11:42:01 +0100

    dev: validation with yup

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { Field, useForm } from "vee-validate";
+import { useForm } from "vee-validate";
+import { object, string } from "yup";
 
 import { EnvelopeIcon } from "@heroicons/vue/24/outline";
 import { ArrowRightEndOnRectangleIcon } from "@heroicons/vue/24/solid";
@@ -10,15 +11,18 @@ interface LoginForm {
   password: string;
 }
 
-const isFormValid = computed(() => {
-  return true;
+const schema = object({
+  email: string().required().email(),
+  password: string().required(),
 });
 
 const authenticationStore = useAuthenticationStore();
-
 const isConnecting = ref(false);
 
-const { handleSubmit } = useForm<LoginForm>();
+const { handleSubmit, errors, defineField, meta } = useForm<LoginForm>({
+  validationSchema: schema,
+});
+const [email, emailAttrs] = defineField("email");
 
 const onSubmit = handleSubmit(async ({ email, password }) => {
   try {
@@ -40,12 +44,17 @@ const onSubmit = handleSubmit(async ({ email, password }) => {
       <form class="flex w-full max-w-xs flex-col gap-2" @submit="onSubmit">
         <label class="input input-bordered flex items-center gap-2">
           <EnvelopeIcon class="size-6 text-gray-500" />
-          <Field name="email" type="email" placeholder="E-mail" />
+          <input
+            v-model="email"
+            type="email"
+            placeholder="E-mail"
+            v-bind="emailAttrs"
+          />
         </label>
         <InputPassword name="password" placeholder="Mot de passe" />
         <ButtonPrimary
           :isRunning="isConnecting"
-          :disabled="isConnecting || !isFormValid"
+          :disabled="isConnecting || !meta.valid"
         >
           <template #icon>
             <ArrowRightEndOnRectangleIcon class="size-6" />
@@ -56,6 +65,7 @@ const onSubmit = handleSubmit(async ({ email, password }) => {
       <NuxtLink to="/new-account" class="link">
         Pas encore de compte ?
       </NuxtLink>
+      <span>{{ errors }}</span>
     </main>
   </ClientOnly>
 </template>
