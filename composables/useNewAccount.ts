a7cb12d59e625215ebeb5cc498d20a0118948a9d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-02-03 18:07:35 +0100

    dev: debounce yup

--- composables/useNewAccount.ts
+++ composables/useNewAccount.ts
@@ -8,15 +8,21 @@ export default function useNewAccount() {
     email: string()
       .required()
       .email()
-      .test("mail-already-taken", "Mail déjà pris", async (value) => {
-        const userStore = useUserStore();
-        await userStore.wait();
-        await sleep(1000);
-        const isExistingEmail = userStore.users.find((u) =>
-          value.startsWith(u.email),
-        );
-        return !isExistingEmail;
-      }),
+      .test(
+        "mail-already-taken",
+        "Mail déjà pris",
+        debounce(1000, async (value) => {
+          console.log("async validation: ", value);
+          const userStore = useUserStore();
+          await userStore.wait();
+          await sleep(1000);
+          const isExistingEmail = userStore.users.find((u) =>
+            value.startsWith(u.email),
+          );
+          console.log("isExistingEmail: ", isExistingEmail);
+          return !isExistingEmail;
+        }),
+      ),
     newPassword: string().required(),
     confirmNewPassword: string()
       .oneOf(
